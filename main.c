fded20ee195825f19892e75c9ca1f284cc9e9f53
András Veres-Szentkirályi <vsza@vsza.hu>
2013-07-21 17:24:59 +0200

    added working version of AVR source

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+
+#define LED_PIN 1
+#define HOLD 16
+
+inline void sw_pwm(uint8_t fill);
+
+int main() {
+	uint8_t level, wait;
+	DDRB |= LED_PIN;
+	while (1) {
+		for (level = 0; level < 255; level++) {
+			for (wait = 0; wait < HOLD; wait++) {
+				sw_pwm(level);
+			}
+		}
+		for (level = 255; level > 0; level--) {
+			for (wait = 0; wait < HOLD; wait++) {
+				sw_pwm(level);
+			}
+		}
+	}
+}
+
+inline void sw_pwm(uint8_t fill) {
+	uint8_t ctr = 0;
+	for (ctr = 0; ctr < fill; ctr++) {
+		asm("nop");
+	}
+	PORTB ^= LED_PIN;
+	for (ctr = fill; ctr != 0; ctr++) {
+		asm("nop");
+	}
+	PORTB ^= LED_PIN;
+}
